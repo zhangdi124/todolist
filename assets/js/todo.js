3b2fac1e552b9eb1eddffc02feb4efed80bfbb36
di <zhangdi8501@gmail.com>
2018-01-02 21:19:51 -0500

    upload files needed

---
+++ assets/js/todo.js
@@ -0,0 +1,28 @@
+//check off items by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//click on x to delete to do
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+})
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new todo text from input
+		var todoText = $(this).val();
+		//clear the input text
+		$(this).val("");
+		//create a new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
+	}
+});
+
+$(".fa-pencil").click(function(){
+	$("input[type='text']").fadeToggle();
+});
+
